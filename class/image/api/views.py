190415461434fff529c17b31108c9a986714b1c4
unknown <aaismail456@gmail.com>
2023-06-23 23:13:38 +0300

    adding labels

--- class/image/api/views.py
+++ class/image/api/views.py
@@ -1,17 +1,43 @@
-import datetime
-import json
 from rest_framework.views import APIView
 from .serializer import *
 from rest_framework.permissions import AllowAny
 from rest_framework.parsers import MultiPartParser
 from django.http import JsonResponse
-from PIL import ImageFile
 import face_recognition
 import os
 import numpy as np
 import joblib
-from django.core.files import File
-from datetime import datetime
+import numpy as np
+class KNN:
+    distances = [[]*2]
+    final_label = []
+    def getDistance(self,test_vector,train_feature_vectors,train_labels): 
+        for i in range(len(train_feature_vectors)):
+            distance = np.sqrt(np.sum((test_vector-train_feature_vectors[i])**2)) #calculate the distance between test vector and train vectors
+            self.distances.append([distance,train_labels[i]]) #append the distance and label to the distances array
+        self.distances = sorted(self.distances, key=lambda x:x[0]) #sort the distances array by the distance
+        return self.distances
+    def getLabel(self,k):
+        labels = []
+        for i in range(k):
+            if self.distances[i][0] < 0.55:
+                labels.append(self.distances[i][1])
+            else:
+                labels.append("UNKNOWN PERSON!")
+        return labels
+
+    def getNearestNeighbor(self,k):
+        labels = self.getLabel(k)
+        return max(set(labels), key=labels.count)
+
+    def Classifier(self,k,train_features, test_features, Trainlabels):
+
+        for i in range(len(test_features)):
+            self.distances = []
+            self.getDistance(test_features[i],train_features,Trainlabels)
+            self.final_label.append(self.getNearestNeighbor(k))
+        return self.final_label
+
 
 
 def getFace(img):
@@ -46,6 +72,7 @@ def load_data(path):
     return Xdata,yLabel
 from rest_framework.parsers import MultiPartParser
 import pandas as pd
+import csv
 class predict_image(APIView):
     permission_classes = [AllowAny]
     parser_classes = [MultiPartParser]
@@ -54,10 +81,20 @@ class predict_image(APIView):
             image_data = request.FILES.get('image')
             image_instance = image(image=image_data)
             image_instance.save()
+            model = KNN()
+            train_data_loaded = joblib.load('D:\\college\\final project\\image\\class\\image\\savedModels\\train_data.joblib')
+            train_label_loaded = joblib.load('D:\\college\\final project\\image\\class\\image\\savedModels\\train_labels.joblib')
             test_data,test_label = load_data(image_instance.image.path)
-            # test_data_list = test_data.tolist()
-            test = pd.Series(test_data).to_json(orient='values')
-            print (test_data)
-            return JsonResponse({'test data' : test})
+            y_pred = model.Classifier(9,train_data_loaded,test_data,train_label_loaded)
+            num = y_pred.count('UNKNOWN PERSON!')
+            merged_list = list(set(y_pred) | set(y_pred))
+            for i in range(num-1):
+                self.merged_list.append('UNKNOWN PERSON!')
+            image_instance.classification_results = merged_list
+            image_instance.save()
+            y_pred.clear()
+            return JsonResponse({'prediction': merged_list})
         else:
             return JsonResponse({'error': 'Invalid request method'})
+
+
